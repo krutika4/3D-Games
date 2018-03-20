22655dfd256d87167f0bdea88d76e0fc253c03a6
Krutika <krutikj@clemson.edu>
2018-03-20 16:27:11 -0400

    Simple 2D platformer developed based on a tutorial from Unity3D website

---
+++ 2D Platformer/Scripts/PlatformFall.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformFall : MonoBehaviour {
+
+	public float fallDelay = 1f;
+
+	private Rigidbody2D rb;
+	// Use this for initialization
+	void Awake () {
+		rb = GetComponent<Rigidbody2D> ();
+		rb.isKinematic = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D (Collision2D other) {
+		if (other.gameObject.CompareTag ("Player")) {
+			Invoke ("Fall", fallDelay);
+		}
+	}
+
+	void Fall() {
+		rb.isKinematic = false;
+	}
+
+}
---
+++ 2D Platformer/Scripts/SpawnManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour {
+
+	public int maxPlatforms = 20;
+	public GameObject platform;
+	public float hMin = 6.5f;
+	public float hMax = 14f;
+	public float vMin = -6f;
+	public float vMax = 6f;
+
+	private Vector2 originPosition;
+
+	// Use this for initialization
+	void Start () {
+		originPosition = transform.position;
+		Spawn ();
+	}
+	
+	void Spawn(){
+		for(int i=0; i<maxPlatforms; i++){
+			Vector2 randomPosition = originPosition + new Vector2(Random.Range(hMin, hMax), Random.Range(vMin, vMax));
+			Instantiate(platform, randomPosition, Quaternion.identity);
+			originPosition = randomPosition;
+		}
+	}
+}
